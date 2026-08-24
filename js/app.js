10e7dbb19884f04011e22b2c5f9e89bfe36a70ce
Irfan Azmi <92423680+irfanahmedazmi@users.noreply.github.com>
2026-08-24 09:05:07 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+fetch('./data/customers.json')
+  .then(response => response.json())
+  .then(customers => {
 
+    const app = document.getElementById('app');
+
+    let html = '<h2>Customers</h2>';
+
+    customers.forEach(customer => {
+      html += `
+        <div style="
+          padding:15px;
+          margin:10px 0;
+          background:#12213b;
+          border-radius:8px;
+          cursor:pointer;
+        ">
+          ${customer.name}
+        </div>
+      `;
+    });
+
+    app.innerHTML = html;
+
+  });
